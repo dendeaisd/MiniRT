9397905a85dcbb4d7ebc6fee53673aa7c6e8a93d
FlaviaV <flavia.voicuu@gmail.com>
2024-06-12 13:01:40 +0200

    refactoring rendering.c'

--- src/rendering.c
+++ src/rendering.c
@@ -6,7 +6,7 @@
 /*   By: fvoicu <fvoicu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 21:53:57 by fvoicu            #+#    #+#             */
-/*   Updated: 2024/06/11 19:15:03 by fvoicu           ###   ########.fr       */
+/*   Updated: 2024/06/12 13:00:22 by fvoicu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ void	setup_camera(t_camera *camera, int width, int height)
 
 bool	check_intersections(t_ray *ray, t_scene *scene)
 {
-	int	i;
-	float t;
+	int		i;
+	float	t;
 
 	i = -1;
 	while (++i < scene->objects_nb)
@@ -37,7 +37,7 @@ bool	check_intersections(t_ray *ray, t_scene *scene)
 
 unsigned int	get_pixel_color(bool hit)
 {
-	if ( hit = true)
+	if (hit == true)
 		return (0xFFFFFFFF);
 	return (0x000000FF);
 }
@@ -54,49 +54,26 @@ void	display_img(t_window *window)
 
 void	render_scene(t_mini_rt *mini_rt)
 {
-	t_window	*window;
-	t_scene		*scene;
+	t_ray			ray;
+	bool			hit;
+	unsigned int	color;
+	int				i;
+	int				j;
 
-	window = mini_rt->window;
-	scene = mini_rt->scene;
+	setup_camera(&mini_rt->scene->camera, \
+			mini_rt->window->width, mini_rt->window->height);
+	j = -1;
+	hit = false;
+	while (++j < mini_rt->window->height)
+	{
+		i = -1;
+		while (++i < mini_rt->window->width)
+		{
+			ray = generate_ray(mini_rt->scene, mini_rt->window, i, j);
+			hit = check_intersections(&ray, mini_rt->scene);
+			color = get_pixel_color(hit);
+			mlx_put_pixel(mini_rt->window->img, i, j, color);
+		}
+	}
+	display_img(mini_rt->window);
 }
-
-
-
-
-
-
-// void render_scene(t_mini_rt *mini_rt)
-// {
-// 	t_window    *window;
-// 	t_scene		*scene;
-	
-// 	window = mini_rt->window;
-// 	scene = mini_rt->scene;
-
-// 	scene->camera.ratio = calc_aspect_ratio(window->width, window->height);
-// 	calc_viewport_dim(&scene->camera, scene->camera.ratio, \
-// 						&scene->camera.viewport);
-// 	for (int y = 0; y < window->height; y++) {
-// 		for (int x = 0; x < window->width; x++) {
-// 			t_ray ray = generate_ray(scene, window, x, y);
-// 			// Check for any intersection
-// 			bool hit = false;
-// 			for (int i = 0; i < scene->objects_nb; i++) {
-// 				float t;
-// 				if (intersect_sphere(&ray, &scene->objects[i].data.sphere, &t)) {
-// 					hit = true;
-// 					break ; // Stop checking if we find a hit
-// 				}
-// 			}
-// 			uint32_t color = hit ? 0xFFFFFFFF : 0x000000FF; // White if hit, black otherwise
-// 			mlx_put_pixel(window->img, x, y, color);
-// 		}
-// 	}
-	
-// 	if (mlx_image_to_window(window->mlx, window->img, 0, 0) == -1) {
-// 		mlx_delete_image(window->mlx, window->img);
-// 		mlx_terminate(window->mlx);
-// 		fprintf(stderr, "Error displaying image\n");
-// 	}
-// }
