6522df8283dc5dfe770b11bd21b663251f77f5c0
FlaviaV <flavia.voicuu@gmail.com>
2024-06-17 12:28:18 +0200

    pixel color mgmt funcs implemented

--- include/miniRT.h
+++ include/miniRT.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   miniRT.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: flaviav <flaviav@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fvoicu <fvoicu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 20:29:12 by fvoicu            #+#    #+#             */
-/*   Updated: 2024/06/11 00:32:48 by flaviav          ###   ########.fr       */
+/*   Updated: 2024/06/17 12:26:55 by fvoicu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,13 +36,14 @@ typedef struct s_mini_rt
 	t_scene			*scene;
 }	t_mini_rt;
 
-t_mini_rt	*init_mini_rt(int width, int height);
-void		destroy_mini_rt(t_mini_rt *mini_rt);
+t_mini_rt		*init_mini_rt(int width, int height);
+void			destroy_mini_rt(t_mini_rt *mini_rt);
 
-t_window	*init_window(int width, int height);
-void		destroy_window(t_window *window);
+t_window		*init_window(int width, int height);
+void			destroy_window(t_window *window);
 
-void		render_scene(t_mini_rt *mini_rt);
+unsigned int	get_pixel_color(int obj_idx, t_scene *scene);
+void			render_scene(t_mini_rt *mini_rt);
 
 /*******************************************************************/
 //TODO: thes funcs dont belong here, header inclusion issue fix it
