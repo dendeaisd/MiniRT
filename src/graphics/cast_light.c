047838efc29dc8938ad215084ec4449397ae2554
FlaviaV <flavia.voicuu@gmail.com>
2024-06-20 02:32:18 +0200

    i think spot brightness is kindof okayish

--- src/graphics/cast_light.c
+++ src/graphics/cast_light.c
@@ -6,12 +6,13 @@
 /*   By: fvoicu <fvoicu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 18:03:24 by fvoicu            #+#    #+#             */
-/*   Updated: 2024/06/20 00:06:04 by fvoicu           ###   ########.fr       */
+/*   Updated: 2024/06/20 01:40:31 by fvoicu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
+//TODO: add clamp color func 
 unsigned int	vec_to_color(t_color color)
 {
 	unsigned int	r;
@@ -49,22 +50,64 @@ static t_color	calc_diffuse_light(t_light light, t_vec hit_point, t_vec normal)
 	});
 }
 
-t_color	cast_light(t_scene *scene, t_color obj_color, \
-					t_vec hit_point, t_vec normal)
-{
-	t_color	ambilight;
-	t_color	diffuse_light;
-	t_color	total_color;
+// t_color	cast_light(t_scene *scene, t_color obj_color, \
+// 					t_vec hit_point, t_vec normal)
+// {
+// 	t_color	ambilight;
+// 	t_color	diffuse_light;
+// 	t_color	total_color;
 	
-	diffuse_light = (t_color){0, 0, 0};
-	ambilight = apply_ambilight(scene->ambilight, obj_color);
-	if (!cast_shadow(scene, hit_point, scene->light, normal))
-		diffuse_light = calc_diffuse_light(scene->light, \
-								hit_point, normal);
-	total_color = (t_color){
-		.r = fmin(ambilight.r + diffuse_light.r, 255),
-		.g = fmin(ambilight.g + diffuse_light.g, 255),
-		.b = fmin(ambilight.b + diffuse_light.b, 255)
+// 	diffuse_light = (t_color){0, 0, 0};
+// 	ambilight = apply_ambilight(scene->ambilight, obj_color);
+// 	if (!cast_shadow(scene, hit_point, scene->light, normal))
+// 		diffuse_light = calc_diffuse_light(scene->light, \
+// 								hit_point, normal);
+// 	total_color = (t_color){
+// 		.r = fmin(ambilight.r + diffuse_light.r, 255),
+// 		.g = fmin(ambilight.g + diffuse_light.g, 255),
+// 		.b = fmin(ambilight.b + diffuse_light.b, 255)
+// 	};
+// 	return (total_color);
+// }
+t_color scale_color(t_color color, float factor) {
+	t_color	result;
+
+	result.r = fmin(255, color.r * factor);
+	result.g = fmin(255, color.g * factor);
+	result.b = fmin(255, color.b * factor);
+	return (result);
+}
+
+float calculate_brightness_factor(t_scene *scene, t_vec hit_point, t_vec normal)
+{
+	t_vec	light_dir = vec_sub(scene->light.position, hit_point);
+	float	distance = vec_len(light_dir);
+	light_dir = light_dir;
+
+	float	dot_product = vec_dot(normal, light_dir);
+	dot_product = fmax(0.0, dot_product);
+
+	float	attenuation = 1.0 / (1.f + 0.07f * distance + 0.005f * distance * distance);
+	return (dot_product * attenuation);
+}
+
+t_color cast_light(t_scene *scene, t_color obj_color, t_vec hit_point, t_vec normal)
+{
+	t_color	ambilight = apply_ambilight(scene->ambilight, obj_color);
+	t_color	diffuse_light = {0, 0, 0};
+
+	if (!cast_shadow(scene, hit_point, scene->light, normal)) {
+		float brightness_factor = calculate_brightness_factor(scene, hit_point, normal);
+		diffuse_light = calc_diffuse_light(scene->light, hit_point, normal);
+		diffuse_light = scale_color(diffuse_light, brightness_factor);
+	}
+	t_color	total_color = {
+		.r = fmin(255, ambilight.r + diffuse_light.r),
+		.g = fmin(255, ambilight.g + diffuse_light.g),
+		.b = fmin(255, ambilight.b + diffuse_light.b)
 	};
+
 	return (total_color);
 }
+
+
